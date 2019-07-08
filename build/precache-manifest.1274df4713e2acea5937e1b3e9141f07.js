aee04946866aba242658820d1c8adf6bf66127bf
Carlos Nieto Tinoco <canietot@unal.edu.co>
2019-07-08 05:38:45 -0500

    Parctica JSX.

---
+++ build/precache-manifest.1274df4713e2acea5937e1b3e9141f07.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a05f9d9c718cd2b87416abbf999d8303",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bece6dd29429f8e102f",
+    "url": "/static/js/2.8a984820.chunk.js"
+  },
+  {
+    "revision": "38ba0f31079c6e087341",
+    "url": "/static/js/main.1f715f94.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import Color from "./Color";
+
+class App extends Component{
+    render(){
+        return(
+            <Color/>   
+        );
+    }
+}
+
+render(<App/>,document.getElementById('root'));
